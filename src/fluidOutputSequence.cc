f7ea64e53196575a8b78b928f9c4999401f1a251
Chris Lahey <clahey@clahey.net>
2011-10-16 16:26:36 -0400

    Cleaned up GetCurrentTime.

--- src/fluidOutputSequence.cc
+++ src/fluidOutputSequence.cc
@@ -20,10 +20,14 @@ FluidOutputSequence::~FluidOutputSequence()
   delete_fluid_sequencer(mSequencer);
 }
 
+#define S_PER_M 60
+#define MS_PER_S 1000
+#define BEATS_PER_BAR 2
+
 TimeDelta
 FluidOutputSequence::GetCurrentTime()
 {
   unsigned int ms_offset = fluid_sequencer_get_tick(mSequencer) - mSequencerBase;
-  return sBar * (ms_offset * mBPM / 120000) + mTicksBase;
+  return TimeDelta::sBar * (ms_offset * mBPM / (S_PER_M * MS_PER_S * BEATS_PER_BAR)) + mTicksBase;
 }
 
