8ea6128a44440e36c94220e017b5c466040de26a
Chris Lahey <clahey@clahey.net>
2012-06-17 14:15:10 -0400

    Allow for empty Chord objects, chords with different notes (thirds, fifths, sevenths,) and extra constructors.

--- src/chord.cc
+++ src/chord.cc
@@ -12,6 +12,80 @@ Chord::Chord(const std::vector<Note>& notes)
   mBase = notes[0].GetMidiNote() % 12;
   mThird = notes[1] - notes[0];
   mFifth = notes[2] - notes[0];
+  mSeventh = -1;
+}
+
+Chord::Chord(const Glib::ustring& description)
+{
+  mBase = -1;
+  mThird = -1;
+  mFifth = -1;
+  mSeventh = -1;
+  if (description.empty()) {
+    return;
+  }
+  int position = 0;
+  int length = description.length();
+  bool minor = false;
+  bool dominant = false;
+  int base = 0;
+
+  base = Note::GetOffset(description[position]);
+  position ++;
+  if (position < length && description[position] == '#') {
+    base ++;
+    position ++;
+  }
+  if (position < length && description[position] == 'm') {
+    minor = true;
+    position ++;
+  }
+  if (position < length && description[position] == '7') {
+    dominant = true;
+    position ++;
+  }
+  if (position != length) {
+    return;
+  }
+  mBase = base;
+  mThird = minor ? 3 : 4;
+  mFifth = 7;
+  if (dominant) {
+    mSeventh = 10;
+  }
+}
+
+
+Chord::Chord(const Glib::ustring& root, int alter, const Glib::ustring& kind)
+{
+  mBase = -1;
+  mThird = -1;
+  mFifth = -1;
+  mSeventh = -1;
+  if (root.bytes() != 1) {
+    return;
+  }
+  bool minor = false;
+  bool minorseventh = false;
+  int base = 0;
+
+  base = (Note::GetOffset(*root.c_str()) + alter) % 12;
+  if (kind == "minor") {
+    minor = true;
+  } else if (kind == "dominant") {
+    minorseventh = true;
+  } else if (kind == "minor-seventh") {
+    minor = true;
+    minorseventh = true;
+  } else if (!kind.empty() && kind != "major") {
+    return;
+  }
+  mBase = base;
+  mThird = minor ? 3 : 4;
+  mFifth = 7;
+  if (minorseventh) {
+    mSeventh = 10;
+  }
 }
 
 Note
@@ -25,12 +99,31 @@ Note
 Chord::GetThird(Note octave)
   const
 {
-  return GetBase(octave) + mThird;
+  if (mThird == -1) {
+    return -1;
+  } else {
+    return GetBase(octave) + mThird;
+  }
 }
 
 Note
 Chord::GetFifth(Note octave)
   const
 {
-  return GetBase(octave) + mFifth;
+  if (mFifth == -1) {
+    return -1;
+  } else {
+    return GetBase(octave) + mFifth;
+  }
+}
+
+Note
+Chord::GetSeventh(Note octave)
+  const
+{
+  if (mSeventh == -1) {
+    return -1;
+  } else {
+    return GetBase(octave) + mSeventh;
+  }
 }
--- src/chord.hh
+++ src/chord.hh
@@ -9,6 +9,7 @@
 #define CHORD_HH_
 
 #include <vector>
+#include <glibmm/ustring.h>
 
 #include "note.hh"
 
@@ -16,17 +17,23 @@ class Chord
 {
 public:
   Chord(const std::vector<Note>& notes);
+  Chord(const Glib::ustring& description = "");
+  Chord(const Glib::ustring& root, int alter, const Glib::ustring& kind);
   Note GetBase(Note octave) const;
   Note GetThird(Note octave) const;
   Note GetFifth(Note octave) const;
+  Note GetSeventh(Note octave) const;
 
   bool operator== (Chord other) const { return mBase == other.mBase && mThird == other.mThird && mFifth == other.mFifth; };
   bool operator!= (Chord other) const { return !(*this == other); };
 
+  bool IsValid() const { return mBase >= 0 && mBase < 12; }
+
 private:
-  int mBase; // 0 - 11, 0 = C, 10 = B flat.
-  int mThird;
-  int mFifth;
+  int mBase; // 0 - 11, 0 = C, 10 = B flat. -1 = invalid.
+  int mThird; // offset or -1 = no third
+  int mFifth; // offset or -1 = no fifth
+  int mSeventh; // offset or -1 = no seventh
 };
 
 #endif /* CHORD_HH_ */
