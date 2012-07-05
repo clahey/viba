ed839183723dd29cd0f27f314db8430accd7d6e8
Jeff Kaufman <jeff@jefftk.com>
2012-07-05 11:12:08 -0400

    emphasize the offbeat

--- src/fiddler.cc
+++ src/fiddler.cc
@@ -16,21 +16,16 @@ Fiddler::FillOutput(std::vector<InstrumentEventPtr>& output, const BarData& bar,
   const std::vector<NoteEvent>& notes = noteData.GetData();
   for (std::vector<NoteEvent>::const_iterator it = notes.begin(); it != notes.end(); it++) {
     const NoteEvent& noteEvent = *it;
-    int vel = 100;
-#if 0
-    if (noteEvent.GetOffset() == 0) {
-      if (bar.mBarNum % 16 == 0) {
-	vel = 127;
-      } else if (bar.mBarNum % 8 == 0) {
-	vel = 105;
-      } else if (bar.mBarNum % 4 == 0) {
-	vel = 90;
-      }
-    }
-#endif
-
     TimeDelta offset = bar.mOffset + noteEvent.GetOffset();
     TimeDelta duration = noteEvent.GetLength()/2;
+    int vel = 85;
+    
+    // emphasize the offbeat
+    if (noteEvent.GetOffset().GetTicks()/720 % 4 == 2) {
+      vel = 127;
+      offset -= 30;
+    }
+
     vel *= state.pVolume.Get(bar.mStart + offset);
       InstrumentEventPtr event = InstrumentEvent::create(noteEvent.GetNote(), duration, offset, mInstrument, vel);
     output.push_back(event);
